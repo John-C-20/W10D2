114eef361c819f38aa663870f37b3c461896c853
John <johncheung1997@gmail.com>
2021-03-02 14:48:25 -0500

    help

---
+++ frontend/clock.jsx
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+export default class Clock extends React.Component {
+    constructor() {
+        
+    }
+
+    render() {
+        return (
+            <h1>Big Johns' Clock</h1>
+        )
+    }
+
+}
+
+
+
